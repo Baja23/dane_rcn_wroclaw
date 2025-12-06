4ec32caabd6949514f5e40131a02fe6b3513aceb
Barbara Składanowska <bskladanowska@gmail.com>
2025-12-06 20:18:28 +0100

    Corrected script for importing db backups

--- import_db.bat
+++ import_db.bat
@@ -10,35 +10,39 @@ set DB_USER=myuser
 set DB_NAME=mydatabase
 :: --------------------
 
-:: Sprawdzenie czy uzytkownik upuscil plik na skrypt
 if "%~1"=="" (
     echo.
     echo [BLAD] Nie wybrano pliku!
     echo Prosze przeciagnac plik .sql na ikone tego skryptu.
-    echo.
     pause
     exit /b
 )
 
 echo.
 echo ========================================================
-echo   UWAGA! Nadpisuje baze danych: %DB_NAME%
-echo   Wgrywany plik: %~n1%~x1
+echo   Importowanie do bazy: %DB_NAME%
+echo   Plik: %~n1%~x1
 echo ========================================================
 echo.
-echo Nacisnij dowolny klawisz, aby rozpoczac IMPORT...
-echo Zamknij to okno, aby anulowac.
-pause >nul
 
+:: --- KROK 1: PROBA UTWORZENIA BAZY (Jesli nie istnieje) ---
+echo [1/2] Sprawdzam czy baza istnieje...
+docker exec %CONTAINER_NAME% createdb -U %DB_USER% %DB_NAME% 2>nul
+
+if %ERRORLEVEL% equ 0 (
+    echo       Utworzono nowa baze danych: %DB_NAME%
+) else (
+    echo       Baza %DB_NAME% juz istnieje (lub inny blad), kontynuuje import...
+)
 echo.
-echo Trwa wgrywanie danych... Prosze czekac.
 
-:: Komenda importu (metoda z 'type' i 'docker exec -i')
+:: --- KROK 2: IMPORT DANYCH ---
+echo [2/2] Wgrywanie danych...
 type "%~1" | docker exec -i %CONTAINER_NAME% psql -U %DB_USER% -d %DB_NAME%
 
 if %ERRORLEVEL% equ 0 (
     echo.
-    echo [SUKCES] Baza danych zostala przywrocona!
+    echo [SUKCES] Operacja zakonczona!
 ) else (
     echo.
     echo [BLAD] Wystapil problem podczas importu.
